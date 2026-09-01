89d18acb561a37f9df41161289a675775b90cfa3
Justin Ovens <jovens@gotunix.net>
2026-09-01 16:53:54 -0400

    Fix: Add ListAll to TaskStore interface

--- internal/core/app.go
+++ internal/core/app.go
@@ -26,6 +26,7 @@ type TaskStore interface {
 	Save(task *Task) error
 	Parse(path string) (*Task, error)
 	GetNextID(date time.Time) (string, error)
+	ListAll() ([]*Task, error)
 }
 
 // App acts as the CQRS orchestrator containing all business logic.
