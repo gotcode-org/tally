d7101458965755da42e99f5b5af18e7d9c9184fb
Justin Ovens <jovens@gotunix.net>
2026-09-01 16:20:44 -0400

    Core: Define the Task domain model with ADO and 7pace tracking fields

--- internal/core/task.go
+++ internal/core/task.go
@@ -1,18 +1,62 @@
 /*
-Copyright (C) 2026 The GotCode Collective
+    Copyright (C) 2026 The GotCode Collective
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 package core
+
+import (
+	"time"
+)
+
+// TaskState represents the current lifecycle state of a task.
+type TaskState string
+
+const (
+	StateOpen   TaskState = "open"
+	StateActive TaskState = "active"
+	StatePaused TaskState = "paused"
+	StateClosed TaskState = "closed"
+)
+
+// Task represents a single unit of work in Tally.
+// This struct maps directly to the YAML frontmatter in our Markdown files.
+type Task struct {
+	// Local Core Metadata
+	ID        string    `yaml:"id"`         // e.g., "20260901.001"
+	Title     string    `yaml:"title"`      // e.g., "Upgrade Redis Cluster"
+	Status    TaskState `yaml:"status"`     // open, active, paused, closed
+	Tags      []string  `yaml:"tags,omitempty"`
+	CreatedAt time.Time `yaml:"created_at"`
+	UpdatedAt time.Time `yaml:"updated_at"`
+
+	// Azure DevOps Metadata
+	ADOID   *int   `yaml:"ado_id,omitempty"`   // Nil if not yet synced to ADO
+	ADOType string `yaml:"ado_type,omitempty"` // e.g., "Story", "Technical Story", "Bug"
+
+	// Time Tracking State
+	TotalSeconds  int        `yaml:"total_seconds"`          // Total time tracked locally
+	SyncedSeconds int        `yaml:"synced_seconds"`         // Time successfully pushed to 7pace
+	ActiveTimer   *time.Time `yaml:"active_timer,omitempty"` // When the timer was started (nil if paused)
+
+	// The Raw Markdown Body
+	// This is explicitly ignored by the YAML parser so we can handle it manually
+	Body string `yaml:"-"` 
+}
+
+// UnsyncedSeconds returns the amount of time that hasn't been pushed to 7pace yet.
+func (t *Task) UnsyncedSeconds() int {
+	return t.TotalSeconds - t.SyncedSeconds
+}
